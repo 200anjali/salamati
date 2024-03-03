702b169b78973c41dfb0782d5389d13902d39fa6
200anjali <2020ugee015@nitjsr.ac.in>
2024-03-03 21:52:24 +0530

    prevent autopkay of videos

--- src/screens/videoPlayer.js
+++ src/screens/videoPlayer.js
@@ -1,70 +1,56 @@
-import YoutubePlayer from 'react-native-youtube-iframe'
-import { View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useRef } from 'react';
+import YoutubePlayer from 'react-native-youtube-iframe';
+import { ScrollView } from 'react-native';
 
-const videoPlayer=()=>{
+const VideoPlayer = () => {
+  const playerRef1 = useRef(null);
+  const playerRef2 = useRef(null);
+  const playerRef3 = useRef(null);
+  const playerRef4 = useRef(null);
 
+  const onReady = () => {
+    // Pause the video immediately after it is loaded
+    playerRef1.current?.pause();
+    playerRef2.current?.pause();
+    playerRef3.current?.pause();
+    playerRef4.current?.pause();
+  };
 
-    return (
-        <ScrollView>
-  <YoutubePlayer
-    // width="560" 
-    height={315} 
-    play={true}
-    videoId={'KVpxP3ZZtAc'}
-  />
+  return (
+    <ScrollView>
+      <YoutubePlayer
+        ref={playerRef1}
+        height={315}
+        play={true}
+        videoId={'k9Jn0eP-ZVg'}
+        onReady={onReady}
+      />
 
-<YoutubePlayer
-    // width="560" 
-    height={315} 
-    play={true}
-    videoId={'k9Jn0eP-ZVg'}
-  />
+      <YoutubePlayer
+        ref={playerRef2}
+        height={315}
+        play={true}
+        videoId={'-V4vEyhWDZ0'}
+        onReady={onReady}
+      />
 
-<YoutubePlayer
-    // width="560" 
-    height={315} 
-    play={true}
-    videoId={'k9Jn0eP-ZVg'}
-  />
+      <YoutubePlayer
+        ref={playerRef3}
+        height={315}
+        play={true}
+        videoId={'Gx3_x6RH1J4'}
+        onReady={onReady}
+      />
 
-
-
-<YoutubePlayer
-    // width="560" 
-    height={315} 
-    play={true}
-    videoId={'k9Jn0eP-ZVg'}
-  />
-
-
-
-<YoutubePlayer
-    // width="560" 
-    height={315} 
-    play={true}
-    videoId={'k9Jn0eP-ZVg'}
-  />
-
-
-
-<YoutubePlayer
-    // width="560" 
-    height={315} 
-    play={true}
-    videoId={'k9Jn0eP-ZVg'}
-  />
-
-
-<YoutubePlayer
-    // width="560" 
-    height={315} 
-    play={true}
-    videoId={'k9Jn0eP-ZVg'}
-  />
-</ScrollView>
-    );
+      <YoutubePlayer
+        ref={playerRef4}
+        height={315}
+        play={true}
+        videoId={'Ww1DeUSC94o'}
+        onReady={onReady}
+      />
+    </ScrollView>
+  );
 };
 
-
-export default videoPlayer;
+export default VideoPlayer;
