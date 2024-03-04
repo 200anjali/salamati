23eae0f341f1eb632bdeb30633b2888cad932e61
200anjali <2020ugee015@nitjsr.ac.in>
2024-03-05 00:13:19 +0530

    open url from notification

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
  * @format
  */
 
-import { Alert, AppRegistry, PermissionsAndroid } from 'react-native';
+import { Alert, AppRegistry, PermissionsAndroid,Linking } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import { initializeApp } from '@react-native-firebase/app';
 import messaging from '@react-native-firebase/messaging';
 import React, { useEffect } from 'react'; // Import useEffect from 'react'
 import PushNotification from 'react-native-push-notification';
+// import notifee from '@notifee/react-native';
 
 const BackgroundMessageHandler = async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
@@ -41,23 +42,57 @@ const requestUserPermission = async () => {
 };
 
 
+const handleNotification = (remoteMessage) => {
+  console.log('Received notification:', remoteMessage);
+
+  if (remoteMessage.notification && remoteMessage.notification.body) {
+    Alert.alert(remoteMessage.notification.title || 'Notification', remoteMessage.notification.body);
+  }
+
+  const action = remoteMessage.data.action;
+  console.log(action);
+  // Check the custom action and take appropriate action
+  switch (action) {
+    case 'OPEN_MAP':
+      if (remoteMessage.data.link) {
+        if (Linking && Linking.openURL) {
+          Linking.openURL(remoteMessage.data.link).catch((err) =>
+            console.error('Error opening URL:', err)
+          );
+        } else {
+          console.error('Linking is not supported on this platform');
+        }
+      }
+      break;
+    // Add more cases for other custom actions if needed
+
+    default:
+      // Default action if no match is found
+      break;
+  }
+};
+
+
 const AppWrapper = () => {
   useEffect(() => {
     requestUserPermission();
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-        console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
-        Alert.alert(remoteMessage.notification.body);
-        PushNotification.localNotification({
-          title: remoteMessage.notification.title,
-          message: remoteMessage.notification.body,
-        });
-    });
+    const unsubscribe = messaging().onMessage(handleNotification);
+
+    // const unsubscribe = messaging().onMessage(async remoteMessage => {
+    //     console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    //     Alert.alert(remoteMessage.notification.body);
+    //     PushNotification.localNotification({
+    //       title: remoteMessage.notification.title,
+    //       message: remoteMessage.notification.body,
+    //     });
+    // });
 
     return unsubscribe;
   }, []);
 
   return <App />;
 };
+
 
 
 initializeApp(); // Initialize Firebase
